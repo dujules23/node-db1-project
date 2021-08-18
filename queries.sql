5a700ef9fd04ae9e7762ccae2344fbb698351f81
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-08-18 12:58:30 -0400

    compeleted sql queries

--- queries.sql
+++ queries.sql
@@ -2,13 +2,29 @@
 
 -- Find all customers with postal code 1010
 
+SELECT customername Customer, postalcode FROM Customers
+WHERE postalcode = 1010
+
 -- Find the phone number for the supplier with the id 11
+
+SELECT Phone FROM Suppliers
+WHERE supplierid = 11
 
 -- List first 10 orders placed, sorted descending by the order date
 
+SELECT orderid, orderdate FROM orders
+ORDER BY orderdate desc
+LIMIT 10
+
 -- Find all customers that live in London, Madrid, or Brazil
 
+
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+
+select * from customers
+
+INSERT into customers (customername, contactname, address, city, postalcode, country) values ("The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "Bag End", "111", "Middle Earth")
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
